1038ce7df8c0be0907166ff96363725fcf6badb1
TAMMISETTI7344 <tammisettiyaswanthprakash@gmail.com>
2026-07-29 22:41:40 +0530

    Fixed CORS

--- backend/main.py
+++ backend/main.py
@@ -9,12 +9,13 @@ app = FastAPI()
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://localhost:5174",
-        "http://localhost:5175",
-    ],
+   allow_origins=[
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://localhost:5174",
+    "http://localhost:5175",
+    "https://ai-resume-analyzer-frontend-ly2.onrender.com",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
